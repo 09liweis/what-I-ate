dcfd3bb2d1ba70379d8b569a08a0b6e0233d4bf8
09liweis <liweisen007@gmail.com>
2017-12-08 22:04:02 +0000

    update script path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     <style>
         body {
             font-family: 'Lato';
@@ -90,12 +89,11 @@
     @if (isset($scripts) && count($scripts) > 0)
         @foreach ($scripts as $script)
             @if ($script == 'google')
-                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA3djfQYTXbB8z3xMgg7ueTIs5j0bSBkS8&libraries=places" async defer></script>
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDk6etZqgI2fVd7CdpsIbJj5WLPRRiKuo&libraries=places" async defer></script>
             @else
-                <script type="text/javascript" src="{{asset($script)}}"></script>
+                <script type="text/javascript" src="{{$script}}"></script>
             @endif
         @endforeach
     @endif
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
